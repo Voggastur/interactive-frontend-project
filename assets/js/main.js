f113eec90af94e77a97bb9d0b7b409619e0424e0
Voggastur <v3x0r_x014@hotmail.com>
2020-04-19 21:52:44 +0000

    new project, player movement added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,43 @@
 $("document").ready(function() {
+    $(".button").click(function(){
+        $(".buttoncontainer").slideUp("slow");
+    });
+
+const background = document.getElementById("background");
+const width = background.innerWidth;
+const height = background.innerHeight;
+
+var spaceship = {
+  top: 750,
+  left: 550,
+};
+
+var explosion = document.getElementById("explosion");
+
+var aliens = [];
+
+var lasers = [];
+
+var rockets = [];
+
+var shockwave = [];
+
+
+document.onkeydown = function (e) {
+  console.log(e.keyCode);
+
+  if (e.keyCode === 37) {
+    spaceship.left = spaceship.left - 10;
+    moveSpaceship();
+  } else if (e.keyCode === 39) {
+    spaceship.left = spaceship.left + 10;
+    moveSpaceship();
+  }
+};
+
+function moveSpaceship() {
+  document.getElementById("spaceship").style.top = spaceship.top + "px";
+  document.getElementById("spaceship").style.left = spaceship.left + "px";
+}
 
 });
