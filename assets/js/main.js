327466f404d2d488fb52d87e7103574db23e2c47
Voggastur <v3x0r_x014@hotmail.com>
2020-04-20 21:18:46 +0000

    added laserweapon, switch function tested to no avail, sidescroll implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,74 @@
-$("document").ready(function() {
-    $(".button").click(function(){
-        $(".buttoncontainer").slideUp("slow");
-    });
+$("document").ready(function () {
+  $(".button").click(function () {
+    $(".buttoncontainer").slideUp("slow");
+    $("#background").show();
+    setInterval(gameLoop, 50);
+    gameLoop();
+  });
 
-const background = document.getElementById("background");
-const width = background.innerWidth;
-const height = background.innerHeight;
+  const background = document.getElementById("background");
+  const height = background.innerHeight;
+  const width = background.innerwidth;
 
-var spaceship = {
-  top: 750,
-  left: 550,
-};
+  var spaceship = {
+    top: 400,
+    left: 100,
+  };
 
-var explosion = document.getElementById("explosion");
+  var lasers = [];
 
-var aliens = [];
+  var aliens = [];
 
-var lasers = [];
+  const fireFrequency = 2;
 
-var rockets = [];
+  document.onkeydown = function (e) {
+    console.log(e.keyCode);
 
-var shockwave = [];
+    if (e.keyCode === 37) {
+      spaceship.left = spaceship.left - 10;
+      moveSpaceship();
+    } else if (e.keyCode === 39) {
+      spaceship.left = spaceship.left + 10;
+      moveSpaceship();
+    } else if (e.keyCode === 40) {
+      spaceship.top = spaceship.top + 10;
+      console.log("DOWN");
+      moveSpaceship();
+    } else if (e.keyCode === 38) {
+      spaceship.top = spaceship.top - 10;
+      console.log("UP");
+      moveSpaceship();
+    } else if (e.keyCode === 17) {
+      lasers.push({
+        top: spaceship.top,
+        left: spaceship.left
+      });
+      console.log("FIRE LASER");
+      drawLasers();
+    }
+  };
 
-
-document.onkeydown = function (e) {
-  console.log(e.keyCode);
-
-  if (e.keyCode === 37) {
-    spaceship.left = spaceship.left - 10;
-    moveSpaceship();
-  } else if (e.keyCode === 39) {
-    spaceship.left = spaceship.left + 10;
-    moveSpaceship();
+  function moveSpaceship() {
+    document.getElementById("spaceship").style.top = spaceship.top + "px";
+    document.getElementById("spaceship").style.left = spaceship.left + "px";
   }
-};
 
-function moveSpaceship() {
-  document.getElementById("spaceship").style.top = spaceship.top + "px";
-  document.getElementById("spaceship").style.left = spaceship.left + "px";
-}
+  function drawLasers() {
+    document.getElementById("lasers").innerHTML = ""; // An empty string to put moving laser images
+    for (var laser = 0; laser < lasers.length; laser += 1) {
+      document.getElementById(
+        "lasers"
+      ).innerHTML += `<div class='laser' style='left:${lasers[laser].left}px;
+    top:${lasers[laser].top}px;'></div>`;
+    lasers[laser].left = lasers[laser].left + 14; // lasers are pasted with template literals and speed is set to 14;
+    }
+    for (var laser = 0; laser < lasers.length; laser += 1) {
+      
+    }
+  }
+
+  function gameLoop() {
+    drawLasers();
+  }
 
 });
